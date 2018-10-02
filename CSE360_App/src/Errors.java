ae376935917af21ff3d5ac16c13ec24f22942ba0
Kristian C <10482949+Kristian-Charboneau@users.noreply.github.com>
2018-10-01 22:10:36 -0700

    Worked on Network.update_paths method and added multiple_end_nodes error code.

--- CSE360_App/src/Errors.java
+++ CSE360_App/src/Errors.java
@@ -3,4 +3,5 @@ public class Errors {
 	public int duplicate_node = 1;
 	public int node_loop = 2;
 	public int orphaned_node = 3;
+	public int multiple_end_nodes = 4;
 }
--- CSE360_App/src/Network.java
+++ CSE360_App/src/Network.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 //TODO
 // Check for duplicate end nodes
+import java.util.LinkedHashSet;
 
 
 public class Network {
@@ -9,19 +10,21 @@ public class Network {
 	private Errors error = new Errors();
 	protected int size; //Number of nodes in network
 	private Node start; //Start node of network
+	private Node end; //End node of network
 	
-	private ArrayList<Path> paths; //Stores all paths in the network.
+	private static ArrayList<Path> paths; //Stores all paths in the network.
 	//Each path is an object that stores a list of path names in order from start to end.
 	//E.G. paths = [Path1, Path2, Path3]
 	
 	private HashMap<String, Node> map; //Keys are node names, values are corresponding node objects
-	private HashMap<Node, ArrayList<Path>> pathMap;
+	private HashMap<Node, ArrayList<Path>> pathMap; //Associates a node with a list of paths
 	
 	
     public Network() {
     	size = 0;
     	paths = new ArrayList<>(); // Array of path objects
     	start = null;
+    	end = null;
     	map = new HashMap<String, Node>();
     	pathMap = new HashMap<Node, ArrayList<Path>>();
     }
@@ -49,18 +52,55 @@ public class Network {
     	}
     	map.put(name, node); //Add new node to map
     	size++; //Increment network size
-    	update_paths(node);
+    	update_paths(node, parents);
     	return error.no_error;
     	
     	//TODO
     }
-    private int update_paths(Node node) {
-    	//List of paths for given node
-    	ArrayList<Path> paths = pathMap.get(node);
+    private int update_paths(Node node, ArrayList<String> parents) {
+    	//Store paths relevant to node
+    	LinkedHashSet<Path> paths = new LinkedHashSet<Path>(); //Use a set to avoid duplicates
+    	
+    	//Loop through parent nodes, adding relevant paths to path set
+    	for(int i=0; i<parents.size();i++) {
+    		Node parent_node = map.get(parents.get(i)); //Get the parent node object    		
+    		paths.addAll(pathMap.get(parent_node)); //Add associated paths to path set
+    	}
+    	
+    	// evaluate each path
+    	Path[] pathArray = new Path[paths.size()]; // create array to iterate through each path
+    	paths.toArray(pathArray); //Copy path linked hash set to the array
+    	for(int i=0; i<pathArray.length;i++) {
+    		Path path = pathArray[i]; //Load Path object
+    		if(!path.append_node(node)) {//Append node to path
+    			return error.duplicate_node;
+    		}
+    	}
+    	//Check if node is the end node
+    	//If node has the same list of paths as Network.paths then it contains all paths
+    	//and is therefore the end node.
+    	if(Network.paths.equals(pathMap.get(node))) {
+    		if(end == null) {//There is no end node set
+    			end = node; //The current node is set as the end node
+    		}
+    		else { //There are multiple end nodes
+    			return error.multiple_end_nodes;
+    		}
+    	}
     	//TODO
     	
     	return error.no_error;
     }
+    
+    public boolean isEmpty() {
+    	return size == 0;
+    }
+    
+    public ArrayList<Path> get_paths() {
+    	return paths;
+    }
+    
+ // The following methods are lower priority as they are not necessary for phase 1.
     
 //    public void insert_node(String name, int duration, ArrayList<String> parents, ArrayList<String> children) {
 //    	//TODO
@@ -76,11 +116,5 @@ public class Network {
 //    	return false; //Placeholder
 //    }
     
-    public boolean isEmpty() {
-    	return size == 0;
-    }
     
-    public ArrayList<Path> get_paths() {
-    	return paths;
-    }
 }
