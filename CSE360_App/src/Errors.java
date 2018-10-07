4ef638f6f2576fbb4267360271a52714f81c9ccb
Kristian C <10482949+Kristian-Charboneau@users.noreply.github.com>
2018-10-07 16:20:25 -0700

    Changed error values to protected, fixed type bug in Network,java

--- CSE360_App/src/Errors.java
+++ CSE360_App/src/Errors.java
@@ -1,7 +1,7 @@
 public class Errors {
-	public int no_error = 0;
-	public int duplicate_node = 1;
-	public int node_loop = 2;
-	public int orphaned_node = 3;
-	public int multiple_end_nodes = 4;
+	protected int no_error = 0;
+	protected int duplicate_node = 1;
+	protected int node_loop = 2;
+	protected int orphaned_node = 3;
+	protected int multiple_end_nodes = 4;
 }
