2682c0f69994b38135086b5c42137b8ce2d3fbfd
Kristian C <10482949+Kristian-Charboneau@users.noreply.github.com>
2018-10-12 14:43:27 -0700

    Fixed content from network-class-refactor not merging properly

--- CSE360_App/src/Errors.java
+++ CSE360_App/src/Errors.java
@@ -4,4 +4,6 @@ public class Errors {
 	protected int node_loop = 2;
 	protected int orphaned_node = 3;
 	protected int multiple_end_nodes = 4;
+	public int multiple_start_nodes = 5;
+	public int invalid_parent_reference = 6;
 }
